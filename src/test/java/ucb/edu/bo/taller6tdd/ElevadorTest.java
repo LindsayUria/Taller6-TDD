a194fed27a856b39d4390f78ab303bf7899a5100
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2023-04-18 17:25:12 -0400

    Segunda prueba - Función para “Crear persona”

--- src/test/java/ucb/edu/bo/taller6tdd/ElevadorTest.java
+++ src/test/java/ucb/edu/bo/taller6tdd/ElevadorTest.java
@@ -20,8 +20,30 @@ public class ElevadorTest {
 
         //2 logica de la prueba
 
-        //3 Verificacion de la preba
+        //3 Verificacion de la prueba
         assertEquals("Cerrado",elevador.puerta());
+    }
+
+    //El programa debe tener una función para “Crear persona”, cuando se invoque a esta función,
+    //el programa deberá crear una persona y colocarla en un piso.
+
+    @Test
+    public void crearPersonaPisoAleatorio() throws Exception {
+        //1 preparacion de la prueba
+        Persona persona=new Persona();
+        //2 logica de la prueba
+        persona.crearPersona();
+        //3 Verificacion de la prueba
+        switch (persona.getPiso()){
+            case(1):
+                assertEquals(1, persona.getPiso());
+                break;
+            case(2):
+                assertEquals(2, persona.getPiso());
+                break;
+            default:
+                assertEquals(3, persona.getPiso());
+        }
     }
 
 }
