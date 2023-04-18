f9cda9161f97c7c83c812e96c1c1818b1741f59d
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2023-04-18 17:21:26 -0400

    Primera prueba - El ascensor al comenzar el día espera con su puerta cerrada

--- src/test/java/ucb/edu/bo/taller6tdd/Elevador.java
+++ src/test/java/ucb/edu/bo/taller6tdd/Elevador.java
@@ -1,5 +1,13 @@
 package ucb.edu.bo.taller6tdd;
 
 public class Elevador {
+    private boolean puerta=false;
+    private boolean ocupado=false;
 
+    public String puerta(){
+        if(puerta)
+            return "Abierto";
+        else
+            return "Cerrado";
+    }
 }
--- src/test/java/ucb/edu/bo/taller6tdd/ElevadorTest.java
+++ src/test/java/ucb/edu/bo/taller6tdd/ElevadorTest.java
@@ -1,4 +1,27 @@
 package ucb.edu.bo.taller6tdd;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ElevadorTest {
+    private Elevador elevador;
+
+    @Before
+    public void before(){
+        elevador=new Elevador();
+    }
+
+    //El ascensor al comenzar el día espera con su puerta cerrada en el primer piso del edificio
+    @Test
+    public void inicioDiaPuertaCerrada() throws Exception {
+        //1 preparacion de la prueba
+
+        //2 logica de la prueba
+
+        //3 Verificacion de la preba
+        assertEquals("Cerrado",elevador.puerta());
+    }
+
 }
