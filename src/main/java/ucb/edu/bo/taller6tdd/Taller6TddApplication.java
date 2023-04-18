38d8ad6ef6710393305b4350d90429ceb6e966a7
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2023-04-18 17:04:55 -0400

    Creando el proyecto base

---
+++ src/main/java/ucb/edu/bo/taller6tdd/Taller6TddApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.taller6tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller6TddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller6TddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ucb/edu/bo/taller6tdd/Taller6TddApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.taller6tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller6TddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
